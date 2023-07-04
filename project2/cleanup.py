bc578958b6745a9866ae4b35e0567a83ce2ca058
Trevor Roberts Jr <Trevor.Roberts.Jr@gmail.com>
2023-07-04 11:17:06 -0400

    begin cleanup. need documentation. update README.md

--- project2/cleanup.py
+++ project2/cleanup.py
@@ -1,14 +1,9 @@
 import configparser
 import psycopg2
-from sql_queries import create_table_queries, drop_table_queries
+from sql_queries import drop_table_queries
 
 def drop_tables(cur, conn):
     for query in drop_table_queries:
-        cur.execute(query)
-        conn.commit()
-
-def create_tables(cur, conn):
-    for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
@@ -20,7 +15,6 @@ def main():
     cur = conn.cursor()
 
     drop_tables(cur, conn)
-    create_tables(cur, conn)
 
     conn.close()
 
--- project2/sql_queries.py
+++ project2/sql_queries.py
@@ -1,12 +1,10 @@
 import configparser
 
-
-# CONFIG
+# Load Config
 config = configparser.ConfigParser()
 config.read('dwh.cfg')
 
-# DROP TABLES
-
+# Drop Tables
 staging_events_table_drop = "DROP TABLE IF EXISTS staging_events;"
 staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs;"
 songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
@@ -15,8 +13,7 @@ song_table_drop = "DROP TABLE IF EXISTS songs;"
 artist_table_drop = "DROP TABLE IF EXISTS artists;"
 time_table_drop = "DROP TABLE IF EXISTS time;"
 
-# CREATE TABLES
-
+# Create Staging Tables
 staging_events_table_create= ("""
 CREATE TABLE staging_events (
   artist varchar(265),
@@ -40,7 +37,6 @@ CREATE TABLE staging_events (
 );
 """)
 
-
 staging_songs_table_create = ("""
 CREATE TABLE staging_songs (
   artist_id varchar(60) NOT NULL SORTKEY,
@@ -55,46 +51,8 @@ CREATE TABLE staging_songs (
   year integer
 );
 """)
-                              
-"""
-### Fact Table
 
-1. **songplays** - records in event data associated with song plays i.e. records with page `NextSong`
-    - *songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent*
-
-songplay_id - autogenerate
-join staging_events on song
-level
-userId for user_id
-sessionId for session_id
-ts for start_time
-location
-userAgent for user_Agent
-join staging_songs on title
-song_id
-artist_id
-
-### Dimension Tables
-
-1. **users** - users in the app
-    - *user_id, first_name, last_name, gender, level* all from staging_events
-2. **songs** - songs in music database
-    - *song_id, title, artist_id, year, duration* all from staging_songs
-3. **artists** - artists in music database
-    - *artist_id, name, location, lattitude, longitude* all from staging_songs
-4. **time** - timestamps of records in **songplays** broken down into specific units
-    - *start_time, hour, day, week, month, year, weekday* all from songplays
-
-### Sample Join
-select le.starttime, d.query, d.line_number, d.colname, d.value,
-le.raw_line, le.err_reason    
-from stl_loaderror_detail d, stl_load_errors le
-where d.query = le.query
-order by le.starttime desc
-limit 100
-
-"""
-
+# Create Fact Table
 songplay_table_create = ("""
 CREATE TABLE songplays (
   songplay_id integer identity(0,1) sortkey,
@@ -108,7 +66,8 @@ CREATE TABLE songplays (
   user_agent varchar(265)
 );
 """)
-
+                         
+# Create Dimension Tables
 user_table_create = ("""
 CREATE TABLE users (
   user_id varchar(10) sortkey,
@@ -141,7 +100,7 @@ CREATE TABLE artists (
 
 time_table_create = ("""
 CREATE TABLE time (
-  start_time bigint,
+  start_time timestamp,
   hour integer,
   day integer,
   week integer,
@@ -151,103 +110,50 @@ CREATE TABLE time (
 );
 """)
 
-# STAGING TABLES
-
+# Populate Staging Tables
 staging_events_copy = ("""
-COPY {} from {}
-credentials 'aws_iam_role={}'
-region 'us-west-2'
-json {};
+COPY {} FROM {}
+CREDENTIALS 'aws_iam_role={}'
+REGION 'us-west-2'
+JSON {};
 """).format("staging_events",config['S3'].get('LOG_DATA'),config['IAM_ROLE'].get('ARN'), config['S3'].get('LOG_JSONPATH'))
 
 staging_songs_copy = ("""
-COPY {} from {}
-credentials 'aws_iam_role={}'
-region 'us-west-2'
-json 'auto';
+COPY {} FROM {}
+CREDENTIALS 'aws_iam_role={}'
+REGION 'us-west-2'
+JSON 'auto';
 """).format("staging_songs",config['S3'].get('SONG_DATA'),config['IAM_ROLE'].get('ARN'))
 
-# FINAL TABLES
-"""
-  start_time bigint,
-  user_id varchar(10),
-  level varchar(30),
-  song_id varchar(60),
-  artist_id varchar(60),
-  session_id integer,
-  location varchar(265),
-  user_agent varchar(265)
-  
-  updated query: select count (*) from staging_events ev left join staging_songs so on ev.song = so.title AND ev.artist = so.artist_name where ev.page = 'NextSong'
-"""
+# Populate Fact Table
 songplay_table_insert = ("""
 INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-(select ev.ts, ev.userId, ev.level, so.song_id, so.artist_id, ev.sessionId, ev.location, ev.userAgent from staging_events ev, staging_songs so where ev.song = so.title);
+(SELECT ev.ts, ev.userId, ev.level, so.song_id, so.artist_id, ev.sessionId, ev.location, ev.userAgent FROM staging_events ev LEFT JOIN staging_songs so ON ev.song = so.title AND ev.artist = so.artist_name WHERE ev.page = 'NextSong');
 """)
 
-"""
-  artist varchar(265),
-  auth   varchar(30),
-  firstName varchar(30),
-  gender varchar(30),
-  itemInSession integer NOT NULL,
-  lastName varchar(30),
-  length real,
-  level varchar(30),
-  location varchar(265),
-  method varchar(30),
-  page varchar(30),
-  registration real,
-  sessionId integer NOT NULL SORTKEY,
-  song varchar(265),
-  status integer,
-  ts bigint,
-  userAgent varchar(265),
-  userId varchar(10) NOT NULL
-
-  1. **users** - users in the app
-    - *user_id, first_name, last_name, gender, level* all from staging_events
-
-  artist_id varchar(60) NOT NULL SORTKEY,
-  artist_latitude real,
-  artist_location varchar(265),
-  artist_longitude real,
-  artist_name varchar(265),
-  duration real,
-  num_songs integer,
-  song_id varchar(60) NOT NULL,
-  title varchar(265),
-  year integer
-2. **songs** - songs in music database
-    - *song_id, title, artist_id, year, duration* all from staging_songs
-3. **artists** - artists in music database
-    - *artist_id, name, location, lattitude, longitude* all from staging_songs
-
-"""
-
+# Populate Dimension Tables
 user_table_insert = ("""
-insert into users
-(select userId, firstName, lastName, gender, level from staging_events);
+INSERT INTO users
+(SELECT DISTINCT userId, firstName, lastName, gender, level FROM staging_events);
 """)
 
 song_table_insert = ("""
-insert into songs
-(select song_id, title, artist_id, year, duration from staging_songs);
+INSERT INTO songs 
+(SELECT DISTINCT song_id, title, artist_id, year, duration FROM staging_songs);
 """)
 
 artist_table_insert = ("""
-insert into artists
-(select artist_id, artist_name, artist_location, artist_latitude, artist_longitude from staging_songs);
+INSERT INTO artists
+(SELECT DISTINCT artist_id, artist_name, artist_location, artist_latitude, artist_longitude FROM staging_songs);
 """)
 
 time_table_insert = ("""
+INSERT INTO time WITH #epochconvert AS (SELECT TIMESTAMP 'epoch' + start_time/1000 * INTERVAL '1 second' as start_time FROM songplays)
+SELECT DISTINCT start_time, EXTRACT(HOUR FROM start_time), EXTRACT(DAY FROM start_time), EXTRACT(WEEK FROM start_time), EXTRACT(YEAR FROM start_time), EXTRACT(WEEKDAY FROM start_time) FROM #epochconvert
 """)
 
-# QUERY LISTS
-
+# Query Lists to Execute in create_tables.py and etl.py
 create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
-insert_table_queries = [user_table_insert, song_table_insert, artist_table_insert] #, time_table_insert]
-
-# insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
